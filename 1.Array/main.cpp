f4aa2fc0de5add7828002b0010702ee1397c13b1
jhhan1023 <jhhan102312@gmail.com>
2024-08-16 09:41:01 +0900

    array

---
+++ 1.Array/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int array[5] = {1, 2, 3, 4, 5};
+    std::cout << "첫 번째 요소: " << array[0] << std::endl;
+    std::cout << "세 번째 요소: " << array[2] << std::endl;
+
+    return 0;
+}
